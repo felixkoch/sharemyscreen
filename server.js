6eddd11cdb257a4b58aac614d49664f8b425e632
Glitch (sharemyscreen) <none>
2019-04-26 19:16:15 +0000

    ✍️🏞 Updated with Glitch

--- server.js
+++ server.js
@@ -20,3 +20,38 @@ app.get('/', function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+var socket = require('socket.io');
+io = socket(listener);
+
+
+  //let socketRoomMap = {};
+  let roomMembers = {};
+
+io.on('connection', (socket) => {
+  console.log(`Angemeldet ist: ${socket.id}`);
+
+  socket.on('JOIN', (data) => {
+    console.log(`JOIN: ${socket.id} joins ${data}`);
+    socket.join(data);
+
+    //socketRoomMap[socket.id] = data;
+    if(typeof roomMembers[data] == 'undefined')
+    {
+      roomMembers[data] = [];
+    }
+    
+    roomMembers[data].push(socket.id);
+
+    socket.emit('MEMBERS', roomMembers[data]);
+    //io.to(data).emit('MEMBERS', roomMembers[data]);
+  });
+
+  socket.on('VIDEO', (data) => {
+    console.log(`Nachricht von: ${socket.id}`);
+    //console.log(data);
+    //console.log(socketRoomMap[socket.id])
+    io.to(data.room).emit('VIDEO', data);
+  });
+});
